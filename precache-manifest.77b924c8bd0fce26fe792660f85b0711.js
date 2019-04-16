1ac3986d2e7c85f2388e36d3096d11decfb2a171
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-04-16 15:00:23 +0530

    Updates

---
+++ precache-manifest.77b924c8bd0fce26fe792660f85b0711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "0de36bcffc1b960103f0",
+    "url": "/solitaire/static/js/main.81885c4f.chunk.js"
+  },
+  {
+    "revision": "895f0793dc6c48339d69",
+    "url": "/solitaire/static/js/2.110139c2.chunk.js"
+  },
+  {
+    "revision": "0de36bcffc1b960103f0",
+    "url": "/solitaire/static/css/main.402d49a4.chunk.css"
+  },
+  {
+    "revision": "df8371b3eb60adf5e695c98cd0adf82b",
+    "url": "/solitaire/index.html"
+  }
+];
